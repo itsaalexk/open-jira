efb7863099e61a1c1be769ff61578dad0490cc37
itsaalexk <alexkononenko6@protonmail.com>
2023-01-24 15:37:12 +0100

    modified package

--- components/ui/ShowField.tsx
+++ components/ui/ShowField.tsx
@@ -1,9 +1,9 @@
-
+import { FC, useState } from 'react';
 import { Button, Box, TextField } from '@mui/material';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
 import CancelIcon from '@mui/icons-material/Cancel';
 import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
-import { FC } from 'react';
+
 
 
 
@@ -12,17 +12,31 @@ interface Props {
 }
 
 export const ShowField:FC<Props> = ({ setShowText}) => {
+
+    const {error, setError} = useState<Boolean>(false)
+    const {form , setForm} = useState<String>("")
+
     const onHandleClick = () =>{
         setShowText(false)
     }
+
+
+    const onHandleError = ({e}) =>{
+        if (e.target.value.length === 0) {setError(true)}
+    }
+
+    const handleChange = ({target:{value}})=>{
+        setForm(value)
+        console.log(form)
+    }
   return (
     <>
-    <TextField  fullWidth sx ={{marginTop:2, marginBottom: 1}} placeholder ="Nueva Entrada" autofocus multiline label ="Nueva Entrada" helperText ="Por favor ingrese un valor"/>
+    <TextField onChange ={handleChange} fullWidth sx ={{marginTop:2, marginBottom: 1}} placeholder ="Nueva Entrada" autofocus multiline label ="Nueva Entrada" helperText ={error && "Ingrese un texto"} error={setError}/>
     <Box display ="flex" justifyContent ="space-between">
         <Button onClick={onHandleClick} variant ="text" color ="error" endIcon={<CancelIcon />}>
             Cancelar
         </ Button>
-        <Button variant ="outlined" color ="secondary" endIcon={<SaveAltIcon />}>
+        <Button onClick ={onHandleError} variant ="outlined" color ="secondary" endIcon={<SaveAltIcon />}>
             Guardar
         </ Button>
     </Box>
